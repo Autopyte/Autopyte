679a963cd154fdc475f16d546382b6e3f835a178
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2023-06-10 20:43:37 +0530

    Build Project Structure

--- autopyte/__main__.py
+++ autopyte/__main__.py
@@ -1,2 +1,6 @@
+#  Copyright (c) 2023 Autopyte - All Rights Reserved
+#  This file is part of the Autopyte App and is released under the "MIT License Agreement"
+#  Please see the LICENSE file that should have been included as part of this package
+
 if __name__ == '__main__':
     print("Hello World")
