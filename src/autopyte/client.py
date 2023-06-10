79b0acda3dff21e4b2e8f08dbd604b711c0f0a7d
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-06-10 21:34:11 +0530

    Added weather fetching function

--- src/autopyte/client.py
+++ src/autopyte/client.py
@@ -17,9 +17,11 @@ GEO_ENDPOINT: str = "/geo/1.0/direct"
 def search_location(q: str) -> Any:
     """
     Search for a location using the OpenWeatherMap API
+
     :param q: The location to search for (e.g. "London")
     :return: The response from the API
     """
+
     url: str = BASE_URL + GEO_ENDPOINT
     params: dict = {
         "q": q,
@@ -36,3 +38,29 @@ def search_location(q: str) -> Any:
         # raise error
         raise Exception("Unable to find location")
 
+
+def fetch_current_weather(lat: float, lon: float) -> Any:
+    """
+    Fetch the current weather for a location using the OpenWeatherMap API
+
+    :param lat: The latitude of the location
+    :param lon: The longitude of the location
+    :return: The response from the API
+    """
+
+    url: str = BASE_URL + WEATHER_ENDPOINT
+    params: dict = {
+        "lat": lat,
+        "lon": lon,
+        "exclude": "minutely,hourly,daily",
+        "appid": API_KEY
+    }
+
+    response: Response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        # return body as json
+        return response.json()
+    else:
+        # raise error
+        raise Exception("Unable to fetch weather data")
