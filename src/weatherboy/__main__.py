8a1f4eaffeefbd1127b73857359aff5dbc0cabf4
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2023-06-11 13:48:36 +0530

    Update Copyright Notice

--- src/weatherboy/__main__.py
+++ src/weatherboy/__main__.py
@@ -1,5 +1,5 @@
 #  Copyright (c) 2023 Autopyte - All Rights Reserved
-#  This file is part of the Autopyte App and is released under the "MIT License Agreement"
+#  This file is part of Project WeatherBoy and is released under the "MIT License Agreement"
 #  Please see the LICENSE file that should have been included as part of this package
 
 from weatherboy.cli import entrypoint
--- src/weatherboy/service.py
+++ src/weatherboy/service.py
@@ -1,4 +1,4 @@
 #  Copyright (c) 2023 Autopyte - All Rights Reserved
-#  This file is part of the Autopyte App and is released under the "MIT License Agreement"
+#  This file is part of Project WeatherBoy and is released under the "MIT License Agreement"
 #  Please see the LICENSE file that should have been included as part of this package
 
