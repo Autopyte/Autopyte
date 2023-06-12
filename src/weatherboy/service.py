615f8f8bf2f9c01ce80abb37c5a7a37305186e4e
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-06-13 01:05:19 +0530

    Added services

--- src/weatherboy/service.py
+++ src/weatherboy/service.py
@@ -3,13 +3,13 @@
 #  Please see the LICENSE file that should have been included as part of this package
 
 from typing import Any
-
+from client import search_location, fetch_current_weather
 from weatherboy.models import City
 
 
 def search_for_cities(query: str) -> list[City]:
-    pass
+    return search_location(query)
 
 
 def get_current_weather(latitude: float, longitude: float) -> Any:
-    pass
+    return fetch_current_weather(latitude, longitude)
