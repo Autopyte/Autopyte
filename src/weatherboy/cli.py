e0f2d9e3dfe2990da107b61a87c3aa7fa4dcd746
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2023-06-13 00:43:39 +0530

    Initial Implementation

--- src/weatherboy/cli.py
+++ src/weatherboy/cli.py
@@ -1,6 +1,27 @@
 #  Copyright (c) 2023 Autopyte - All Rights Reserved
 #  This file is part of Project WeatherBoy and is released under the "MIT License Agreement"
 #  Please see the LICENSE file that should have been included as part of this package
+from typing import Any
+
+import questionary
+
+from weatherboy.models import City
+from weatherboy.service import search_for_cities, get_current_weather
+
 
 def entrypoint() -> None:
-    print("Hello World")
+    query: str = questionary.text("Enter the city name").ask()
+
+    cities: list[City] = search_for_cities(query)
+    selected_city_name: str = questionary.select(
+        "Choose one", [f"{city.name}, {city.state}, {city.country}" for city in cities]
+    ).ask()
+
+    for city in cities:
+        if f"{city.name}, {city.state}, {city.country}" == selected_city_name:
+            latitude: float = city.lat
+            longitude: float = city.lon
+            break
+
+    weather: Any = get_current_weather(latitude, longitude)
+    print(weather)
--- src/weatherboy/models.py
+++ src/weatherboy/models.py
@@ -2,14 +2,12 @@
 #  This file is part of Project WeatherBoy and is released under the "MIT License Agreement"
 #  Please see the LICENSE file that should have been included as part of this package
 
-from typing import Any
-
-from weatherboy.models import City
+from pydantic import BaseModel
 
 
-def search_for_cities(query: str) -> list[City]:
-    pass
-
-
-def get_current_weather(latitude: float, longitude: float) -> Any:
-    pass
+class City(BaseModel):
+    name: str
+    state: str
+    country: str
+    lat: float
+    lon: float
