3f2f4cc56fe7cd83cdd40ed3e873b6fdeb1cd712
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-06-13 01:10:26 +0530

    Added services

--- src/weatherboy/service.py
+++ src/weatherboy/service.py
@@ -2,13 +2,17 @@
 #  This file is part of Project WeatherBoy and is released under the "MIT License Agreement"
 #  Please see the LICENSE file that should have been included as part of this package
 
-from typing import Any
+from typing import Any, List
 from client import search_location, fetch_current_weather
 from weatherboy.models import City
 
 
 def search_for_cities(query: str) -> list[City]:
-    return search_location(query)
+    city_list_json = search_location(query)
+    city_list: list[City] = []
+    for city in city_list_json:
+        city_list.append(City(city["name"], city["state"], city["country"], city["lat"], city["lon"]))
+    return city_list
 
 
 def get_current_weather(latitude: float, longitude: float) -> Any:
